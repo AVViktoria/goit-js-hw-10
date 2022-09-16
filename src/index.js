004fe20d3e202587a8f197cf33e689ce519d274e
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2022-09-15 23:23:34 -0700

    made fetchCountries function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './css/styles.css';
+import './js/fetchCountries';
 // import { Notify } from 'notiflix/build/notiflix-notify-aio';
 // import { useDebounce } from 'use-lodash-debounce'
+
 
 
 const DEBOUNCE_DELAY = 300;
---
+++ src/js/get-refs.js
@@ -0,0 +1,7 @@
+export default function getRefs() {
+  return {
+    searchForm: document.querySelector('#search-box'),
+    listForm: document.querySelector('.country-list'),
+    infoContainer: document.querySelector('.country-info'),
+  };
+}
